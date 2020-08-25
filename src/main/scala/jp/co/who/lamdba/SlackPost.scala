9b5f6801a458f31d66aea9deaf882ea944d5db3c
Keyno <kofujiw@gmail.com>
2020-08-26 02:09:32 +0900

    update scala sample. 動くところまで修正

--- src/main/scala/jp/co/who/lamdba/SlackPost.scala
+++ src/main/scala/jp/co/who/lamdba/SlackPost.scala
@@ -12,11 +12,11 @@ class SlackPost extends SampleBase {
 }
 
 trait SampleBase
-  extends RequestHandler[Int, java.util.List[String]] {
+  extends RequestHandler[Int, String] {
 
-  override def handleRequest(myCount: Int, context: Context): java.util.List[String] = {
+  override def handleRequest(myCount: Int, context: Context): String = {
     val logger: LambdaLogger = context.getLogger
     logger.log(s"received : ${myCount}")
-    java.util.List.of(myCount.toString)
+    myCount.toString
   }
 }
