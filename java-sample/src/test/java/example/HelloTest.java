ae053c7fc20f2882d2d255268ea984ba77ecdad0
Keyno <kofujiw@gmail.com>
2020-08-27 06:47:15 +0900

    Java のテスト追加

---
+++ java-sample/src/test/java/example/HelloTest.java
@@ -0,0 +1,34 @@
+package example;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.LambdaLogger;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.platform.runner.JUnitPlatform;
+import org.junit.runner.RunWith;
+
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.mock;
+
+@RunWith(JUnitPlatform.class)
+public class HelloTest {
+
+    Context cxt;
+
+    LambdaLogger logger;
+
+    @BeforeEach
+    void setUp() {
+        cxt = mock(Context.class);
+        logger = mock(LambdaLogger.class);
+        doReturn(logger).when(cxt).getLogger();
+    }
+
+    @Test
+    void sample() {
+        Hello test = new Hello();
+        String actual = test.handleRequest(1, cxt);
+        Assertions.assertEquals("1", actual);
+    }
+}
---
+++ java-sample/src/test/java/example/Junit5Test.java
@@ -0,0 +1,11 @@
+package example;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class Junit5Test {
+    @Test
+    void sample() {
+        Assertions.assertEquals(8, 8);
+    }
+}
